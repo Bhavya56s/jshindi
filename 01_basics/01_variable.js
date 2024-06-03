de2de4c4f4af225ca0244de60c58f90f7bc0921b
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 06:25:14 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553 
+let email = "dhruv@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+// accountId = 2
+
+email = "ayush@gmail.com"
+accountPassword = "1234995"
+accountCity = "Bangalore"
+console.log(accountId)
+
+console.table([accountId,accountPassword,email,accountCity])
+/*
+Dont use var because of issue in block scope and function scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code in new versions
+
+//alert("Hello") // we are using node js not browser
+
+
+let name = "Bhavya"
+let age = 23
+console.log(typeof name)
+
+
+// number
+// string = ""
+// bollean = true/false
+// bigint
+// null = standalone value
+// undefine = value assign nhi huii
+// symbol = unique
+
+// object
