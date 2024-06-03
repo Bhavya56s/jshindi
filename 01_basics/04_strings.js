c500bf3d25f5a243be199387d01a83fa8677e559
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 10:33:59 +0000

    string

---
+++ 01_basics/04_strings.js
@@ -0,0 +1,26 @@
+const name="Hitesh"
+const value = 35
+
+console.log(name+value)
+
+console.log(`Hey my name is ${name} and my age is ${value}`)
+
+const gamename = new String("BhavyaaSh")
+
+console.log(gamename[0])
+
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(4))
+console.log(gamename.indexOf('y'))
+
+const newS = gamename.substring(0,4)
+console.log(newS)
+
+const another = gamename.slice(-1,-4)
+console.log(another)
+
+const Str = "    BHavya   "
+console.log(Str)
+console.log(Str.trim())
+
