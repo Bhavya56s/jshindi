6053aa718e26c482139977ef6990ba6c10458b4f
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 12:16:24 +0000

    Date_time

---
+++ 01_basics/06_date_time.js
@@ -0,0 +1,28 @@
+const myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleTimeString())
+
+// let createdate = new Date(2023,0,4)
+
+// console.log(createdate.toString())
+// let mycrDate = new Date("12-04-2023")
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(mycrDate.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+
+console.log(newDate.getDay())
+
+
+
