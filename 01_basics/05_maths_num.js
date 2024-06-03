eef8eaaf8374a08389eead7bf4f4d8ba51ce80b3
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 11:23:53 +0000

    maths_num

---
+++ 01_basics/05_maths_num.js
@@ -0,0 +1,27 @@
+// const score = 100
+// console.log(score);
+
+// const balnce = new Number(1000)
+// console.log(balnce);
+// console.log(balnce.toFixed(2))
+
+// const num = 1234.478
+
+// console.log(num.toPrecision(4))
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+// *************Maths********
+
+// console.log(Math.abs(-5));
+// console.log(Math.round(9.4));
+// console.log(Math.floor(3.8));
+// console.log(Math.ceil(3.4));
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 1
+const max = 6
+console.log(Math.floor(Math.random()*(max-min+1)+min))
