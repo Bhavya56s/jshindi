5dc5f5a3af58d373d1828726555badedea3e6941
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 09:27:35 +0000

    Datatype_summary

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,53 @@
+// let Score = "44abc"
+
+// console.log(typeof Score)
+// console.log(typeof(Score))
+
+
+// let valueInNumber = Number(Score)
+// console.log(typeof valueInNumber)
+
+// console.log(valueInNumber)
+
+// "33" = 33
+// "33a" = NaN
+// true = 1,false = 0
+
+// let isLoggedIn = "Bhavya"
+
+// let boleeanisLoggedIn = Boolean(isLoggedIn)
+// console.log(boleeanisLoggedIn)
+
+// 1 = true,0 = false
+// "" = false ,"hjuj"= true
+
+// let sNumber = 33
+
+// let change = String(sNumber)
+// console.log( typeof change)
+
+// ************Operations************
+
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue)
+
+// let str1 = "Hello"
+// let str2 = " Bhavya"
+// let str3 = str1 + str2
+// console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+3)
+console.log(1+2+"3")
+
+// agar string last mai hai toh ye pehle walo ko number mai consider kr
+// lega or agar string 1 mai hai toh ye sbko string lega
+
+console.log(+true)
+console.log(+"")
+
+let abc=1;
+abc++;
+
+console.log(abc)
---
+++ 01_basics/04_datatypes_summary.js
@@ -0,0 +1,14 @@
+const id = Symbol('123')
+const name = Symbol('123')
+
+
+console.log(id===name)
+
+let myobj = {
+    name :"Bhavya",
+    age : 20
+
+}
+const MyFunction = function(){
+    console.log("Hellow world")
+}
