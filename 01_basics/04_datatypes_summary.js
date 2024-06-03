7f586f8a17079caeca08ea437dd12e1bb81eb2da
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-03 09:53:11 +0000

    summary

--- 01_basics/04_datatypes_summary.js
+++ 01_basics/04_datatypes_summary.js
@@ -1,14 +1,40 @@
-const id = Symbol('123')
-const name = Symbol('123')
+// const id = Symbol('123')
+// const name = Symbol('123')
 
 
-console.log(id===name)
+// console.log(id===name)
 
-let myobj = {
-    name :"Bhavya",
-    age : 20
+// let myobj = {
+//     name :"Bhavya",
+//     age : 20
 
+// }
+// const MyFunction = function(){
+//     console.log("Hellow world")
+// }
+
+
+
+// *******************************************
+
+// Stack (Primitive) , Heap (Non Prmitive)
+
+
+let myname = "Bhavya"
+
+let anname = myname
+anname = "Chirag"
+
+console.log(anname)
+console.log(myname)
+
+let user = {
+    email : "user@google.com",
+    pass : "1234"
 }
-const MyFunction = function(){
-    console.log("Hellow world")
-}
+let usertwo = user
+
+usertwo.email="user2@google.com"
+
+console.log(user)
+console.log(usertwo)
