75371d41613317524eb4d6d7118aa040f229eb4a
Bhavya Singhal <163103865+Bhavya56s@users.noreply.github.com>
2024-06-04 11:20:28 +0000

    arrow

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,47 @@
+const details = {
+    username : "Bhavya",
+    age : 19,
+    welcome : function() {
+        console.log(`Welcome , ${this.username}`)
+        console.log(this)
+    }
+
+}
+// details.welcome()
+// details.username = 'Sam'
+// details.welcome()
+// console.log(this)
+
+// function chai(){
+//     let username = "Bhavya"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai = function (){
+//     let username = "Bhavya"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai =  ()=>{
+//     let username = "Bhavya"
+//     console.log(this.username)
+// }
+// chai()
+
+// const add2 = (num1,num2) => {
+//     return num1 + num2
+// }
+
+// console.log(add2(5,6))
+
+// const add2 = (num1,num2) =>  num1 + num2
+// const add2 = (num1,num2) =>  (num1 + num2)
+// console.log(add2(5,6))
+
+
+( (name) => {
+    console.log(`Bhavya, ${name}`);
+    
+} ) ('hitsh');
